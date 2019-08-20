ea400f28261f416f98e0e505c2763b7eb0bb9fb5
redlout <505665119@qq.com>
2019-08-20 17:52:38 +0800

    test

---
+++ src/main/java/com/pt/myprj/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.pt.myprj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("api/user")
+public class UserController {
+    @RequestMapping(value = "hello", method =   RequestMethod.GET)
+    @ResponseBody
+    public Map login(HttpSession httpSession){
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        try {
+            map.put("errorCode", 0);
+            map.put("message", "hello");
+        } catch (Exception e) {
+            map.put("errorCode", 1);
+            map.put("errorMessage", "未知错误");
+        }
+        return map;
+    }
+}
